a5a1ae896f2730125e83bef86925a8b6620f7d3b
Rodney Bejabeng <Rodneyb2@illinois.edu>
2022-08-09 16:06:15 -0500

    All icons present

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,30 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import TempLogo from '../../assets/images/bitcoin.svg'
+import LogoSub from '../../assets/images/my-logo.png'
 import Home from '../../assets/images/house-chimney.svg'
 import User from '../../assets/images/user.svg'
+import Envelope from '../../assets/images/envelope.svg'
 
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
-            <img src= { Home } alt='logo' class= 'filter-gray' />
+            <img src= { TempLogo } alt='logo' class= 'filter-blue' />
+            <img src= { LogoSub } alt='sub-logo' class= 'filter-gray' />
         </Link>
         <nav>
             <NavLink exact='true' activeclassname='active' to='/'>
+                <img src= { Home } alt='logo' class='filter-gray' />
+                <>Home</>
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' to='/about'>
                 <img src= { User } alt='logo' class='filter-gray' />
+                <>New User</>
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' to='/contact'>
+                <img src= { Envelope } alt='logo' class='filter-gray' />
+                <>Contact Us</>
             </NavLink>
         </nav>
     </div>
