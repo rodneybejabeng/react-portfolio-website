bdc2d5f407c0403e22ac5b09f315b7ba67b43055
Rodney Bejabeng <Rodneyb2@illinois.edu>
2022-08-08 19:10:32 -0500

    add source images

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
 import './index.scss'
+import LogoR from '../../assets/images/logo-r.png'
+import LogoRsubtitle from '../../assets/images/logo_sub.png'
 
-const Sidebar = () => {
-    return <>Hello from the Sidebar</>
-}
+const Sidebar = () => (
+    <div>
+        <Link>
+            <img src= {LogoR} alt='logo' />
+            <img src= {LogoRsubtitle} alt='logo' />
+        </Link>
+    </div>
+)
 
 export default Sidebar;
